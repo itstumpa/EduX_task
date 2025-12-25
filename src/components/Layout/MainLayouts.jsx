7e94252b911029869cd54cbe340c23d878fabcc2
itstumpa <tumpadas1fiverr@gmail.com>
2025-12-25 17:14:06 +0600

    ScrollToTop. fix

--- src/components/Layout/MainLayouts.jsx
+++ src/components/Layout/MainLayouts.jsx
@@ -2,12 +2,14 @@
 import { Outlet } from "react-router";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
+import ScrollToTop from "../ScrollToTop";
 
 const MainLayouts = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <div className="flex-1">
+        <ScrollToTop/>
         <Outlet />
       </div>
       <Footer />
